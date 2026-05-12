a729f28eacbe154f241842c846611ff4dde7af0f
vanessamzuniga-beep <vanessamzuniga@gmail.com>
2026-05-12 10:39:58 -0500

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+// Declare global variables
+const burgers = ['Hamburger', 'Cheeseburger']
+let featuredDrink = 'Strawberry Milkshake'
+
+// create a function that adds new burger to burgers array
+function addBurger(){
+    const newBurger = 'Flatburger'
+    burgers.push(newBurger)
+}
+
+if(true){
+    const anotherNewBurger = 'Maple Bacon Burger'
+    burgers.push(anotherNewBurger)
+}
+
+// function to change featured drink variable
+function changeFeaturedDrink(){
+    featuredDrink = 'The JavaShake'
+}
+
